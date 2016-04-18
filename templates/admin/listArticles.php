f754e8fd217992d1d307133adf613728bdc9a88c
sarthak96 <sarthakm27@gmail.com>
2016-04-18 15:35:26 +0530

    renamed admin.php

--- templates/admin/listArticles.php
+++ templates/admin/listArticles.php
@@ -2,7 +2,7 @@
 
       <div id="adminHeader">
         <h2>Widget News Admin</h2>
-        <p>You are logged in as <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="admin.php?action=logout"?>Log out</a></p>
+        <p>You are logged in as <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="login.php?action=logout"?>Log out</a></p>
       </div>
 
       <h1>All Articles</h1>
@@ -24,7 +24,7 @@
 
 <?php foreach ( $results['articles'] as $article ) { ?>
 
-        <tr onclick="location='admin.php?action=editArticle&amp;articleId=<?php echo $article->id?>'">
+        <tr onclick="location='login.php?action=editArticle&amp;articleId=<?php echo $article->id?>'">
           <td><?php echo date('j M Y', $article->publicationDate)?></td>
           <td>
             <?php echo $article->title?>
@@ -37,7 +37,7 @@
 
       <p><?php echo $results['totalRows']?> article<?php echo ( $results['totalRows'] != 1 ) ? 's' : '' ?> in total.</p>
 
-      <p><a href="admin.php?action=newArticle">Add a New Article</a></p>
+      <p><a href="login.php?action=newArticle">Add a New Article</a></p>
 
 <?php include "templates/include/footer.php" ?>
 
