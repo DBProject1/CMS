2a02ec5953f5b548bd56190a6e97e582b5057dc3
anweshknayak <anweshknayak@gmail.co>
2016-04-19 17:36:42 +0530

    yolo

--- register.php
+++ register.php
@@ -1,49 +1,47 @@
 <?php
 
 require( "config.php" );
+session_start();
 
-
-if(isset($_POST['action']))
+if(isset($_POST["register"]))
 {
-  if($_POST['action']=="regis")
   register1();
 }
 
-function register1()  {
-    //echo "hello";
-    if(isset($_POST['regist']))
-    {
 
+function register1()  {
+
+  echo "hello";
     $results = array();
     $results['pageTitle'] = "User Registration";
 
 
-      $username = $_POST['username1'];
-      $password = $_POST['password1'];
-
       $sql = "INSERT INTO users (name,password,rolecode) VALUES(:name,:password,:user)";
 
 
+      try
+      {
           $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD);
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           $stmt = $conn->prepare( $sql );
-          $stmt->bindValue( ":name", $_POST['username1'], PDO::PARAM_STR );
-          $stmt->bindValue( ":password", $_POST['password1'], PDO::PARAM_STR );
+          $stmt->bindValue( ":name", $_POST["username"], PDO::PARAM_STR );
+          $stmt->bindValue( ":password", $_POST["password"], PDO::PARAM_STR );
           $stmt->bindValue( ":user", "user", PDO::PARAM_STR );
           $stmt->execute();
-
-
-
-          require( TEMPLATE_PATH . "/admin/user.php" );
-
         }
+
+        catch(Exception $e)
+        {
+          echo "Error".$e;
+        }
+
+
 }
 ?>
-<div>
 <?php include "templates/include/header.php" ?>
 
-      <form action="register.php?action=regis" method="post" style="width: 50%;">
-        <input type="hidden" name="regist" value="true" />
+      <form action="register.php?action=register" method="post" style="width: 50%;">
+        <input type="hidden" name="register" value="true" />
 
 <?php if ( isset( $results['errorMessage'] ) ) { ?>
         <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
@@ -53,21 +51,20 @@ function register1()  {
 
           <li>
             <label for="username">Username</label>
-            <input type="text" name="username" id="username1" placeholder="Your user name" required autofocus maxlength="20" />
+            <input type="text" name="username" id="username" placeholder="Your user name" required autofocus maxlength="20" />
           </li>
 
           <li>
             <label for="password">Password</label>
-            <input type="password" name="password" id="password1" placeholder="Your user password" required maxlength="20" />
+            <input type="password" name="password" id="password"  placeholder="Your user password" required maxlength="20" />
           </li>
 
         </ul>
 
         <div class="buttons">
-          <input type="submit" name="register" value="SignUp" />
+          <input type="submit" name="regist" value="SignUp" />
         </div>
 
       </form>
 
 <?php include "templates/include/footer.php" ?>
-</div>
