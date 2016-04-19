aee1d84124a46050f2105b85a5fb65aafc6b4207
anweshknayak <anweshknayak@gmail.co>
2016-04-19 15:39:12 +0530

    userform

--- templates/admin/loginForm.php
+++ templates/admin/loginForm.php
@@ -3,6 +3,7 @@
       <form action="login.php?action=login" method="post" style="width: 50%;">
         <input type="hidden" name="login" value="true" />
 
+
 <?php if ( isset( $results['errorMessage'] ) ) { ?>
         <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
 <?php } ?>
@@ -22,10 +23,18 @@
         </ul>
 
         <div class="buttons">
-          <input type="submit" name="login" value="Login" />
+          <input id = "login" type="submit" name="login" value="Login" />
         </div>
 
+        <div class="buttons">
+          <input id = "regist" type="submit"  name="regist" value="Register" />
+          </div>
+          <script type="text/javascript">
+          document.getElementById("regist").onclick = function () {
+            location.href = "/blog/register.php";
+          };
+          </script>
       </form>
 
-<?php include "templates/include/footer.php" ?>
 
+<?php include "templates/include/footer.php" ?>
