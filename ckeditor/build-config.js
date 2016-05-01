24405e3a30a908416735e18161f194df42809839
sarthak96 <sarthakm27@gmail.com>
2016-05-02 02:05:33 +0530

    changes

--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/d1609cf7a83b12b8190fbc090ea648bf
+ * (2) http://ckeditor.com/builder/3a3f2a3eea715a867597d82d77b65437
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/d1609cf7a83b12b8190fbc090ea648bf
+ * (3) http://ckeditor.com/builder/download/3a3f2a3eea715a867597d82d77b65437
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,7 +25,7 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'flat',
+	skin: 'moono',
 	preset: 'standard',
 	ignore: [
 		'.bender',
@@ -52,14 +52,18 @@ var CKBUILDER_CONFIG = {
 	plugins : {
 		'a11yhelp' : 1,
 		'about' : 1,
+		'autoembed' : 1,
+		'autogrow' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
+		'bt_table' : 1,
 		'clipboard' : 1,
 		'codesnippet' : 1,
 		'contextmenu' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'fastimage' : 1,
 		'filebrowser' : 1,
 		'find' : 1,
 		'floatingspace' : 1,
@@ -67,29 +71,30 @@ var CKBUILDER_CONFIG = {
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
+		'image2' : 1,
+		'imageresponsive' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
-		'pbckcode' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
 		'scayt' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
-		'symbol' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
+		'templates' : 1,
+		'tliyoutube2' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
+		'widgetcommon' : 1,
 		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
--- templates/viewArticle.php
+++ templates/viewArticle.php
@@ -2,7 +2,7 @@
 <HEAD>
 <body background="bg.jpg">
 <?php include "templates/include/header.php" ?>
-
+      <link rel="stylesheet" href="/highlighter/styles/default.css">
       <h1 style="width: 75%;"><?php echo htmlspecialchars( $results['article']->title )?></h1>
       <div style="width: 75%; font-style: italic;"><?php echo htmlspecialchars( $results['article']->summary )?></div>
       <div style="width: 75%;"><?php echo $results['article']->content?></div>
@@ -14,7 +14,8 @@
       <p class="pubDate">Published on <?php echo date('j F Y', $results['article']->publicationDate)?></p>
 
       <p><a href="./">Return to Homepage</a></p>
-
+<script src="/highlighter/highlight.pack.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 <?php include "templates/include/footer.php" ?>
 </body>
 </html>
